03634487671d1a2f5ad664b8e8c0324703d52940
neehhaa <nehawasnik95@outlook.com>
2022-07-11 13:00:23 +0530

    Kilometer to Miles.

---
+++ Day8.py
@@ -0,0 +1,12 @@
+"""
+
+Day8 - Python program to convert kilometres to miles.
+
+"""
+
+kilometer = int(input("Enter kilometers : "))
+
+miles = kilometer * 0.621371
+
+print(kilometer, "Kilometer to miles is : ", miles)
+
