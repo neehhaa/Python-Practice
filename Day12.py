bbfafb2c96889247e112905538f511add42d6f7d
neehhaa <nehawasnik95@outlook.com>
2022-07-11 13:02:31 +0530

    Odd or Even Number.

---
+++ Day12.py
@@ -0,0 +1,12 @@
+"""
+
+Day12 - Python Program to Check if a Number is Odd or Even.
+
+"""
+
+num = int(input("Enter the number : "))
+
+if num % 2 == 0:
+    print(num, " is EVEN.")
+else:
+    print(num, " is ODD")
