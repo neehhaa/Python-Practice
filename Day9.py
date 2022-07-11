38ae835b5ba603d64c425c38f1e7fae7032757d5
neehhaa <nehawasnik95@outlook.com>
2022-07-11 13:01:02 +0530

    Celsius to Fahrenheit.

---
+++ Day9.py
@@ -0,0 +1,12 @@
+"""
+
+Day9 - Python program to convert Celsius to Fahrenheit.
+
+"""
+
+
+celsius = int(input("Enter celsius to get Fahrenheit value : "))
+
+fahrenheit = (celsius * 9 / 5) + 32
+
+print(celsius, " celsius is equal to", fahrenheit, " fahrenheit")
