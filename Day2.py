9ba5985e0c9106a1bce4a9b21328ef53b0ac11d5
neehhaa <nehawasnik95@outlook.com>
2022-07-11 12:56:55 +0530

    Arithmetical Operations

---
+++ Day2.py
@@ -0,0 +1,8 @@
+# Day 2 - Python program to do arithmetical operations.
+
+num1 = 10
+num2 = 20
+print(num1 + num2)  # 30
+print(num1 - num2)  # -10
+print(num1 * num2)  # 200
+print(num1 / num2)  # 0.5
