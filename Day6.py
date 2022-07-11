f950c89e778ae5a5db2f4d055f388618b4703b0a
neehhaa <nehawasnik95@outlook.com>
2022-07-11 12:59:11 +0530

    Swapping Two Numbers.

---
+++ Day6.py
@@ -0,0 +1,15 @@
+""""
+
+Day6 - Python program to swap two variables.
+
+"""
+num1 = int(input("Enter First Number : "))
+num2 = int(input("Enter Second Number : "))
+
+swap = num1
+num1 = num2
+num2 = swap
+
+
+print("Swapped value is :", num1, num2)
+
