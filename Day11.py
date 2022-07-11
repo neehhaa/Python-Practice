ad80f67d6e5b7436c325e5a20369aa2451467b64
neehhaa <nehawasnik95@outlook.com>
2022-07-11 13:02:09 +0530

    Positive, Negative or Zero Number.

---
+++ Day11.py
@@ -0,0 +1,14 @@
+"""
+
+Day11 - Python Program to Check if a Number is Positive, Negative or Zero.
+
+"""
+
+
+number = int(input("Enter the number : "))
+if number > 0:
+    print("Positive Number")
+elif number == 0:
+    print("Zero")
+else:
+    print("Negative Number")
